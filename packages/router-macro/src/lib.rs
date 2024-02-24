2dce2303a8113397ba6928e75651462a358bb919
Evan Almloff <evanalmloff@gmail.com>
2024-02-24 11:22:30 -0600

    Revert "Fix Clippy warning in `Routable` macro"

--- packages/router-macro/src/lib.rs
+++ packages/router-macro/src/lib.rs
@@ -559,7 +559,6 @@ impl RouteEnum {
             #(#type_defs)*
 
             #[allow(non_camel_case_types)]
-            #[allow(clippy::derive_partial_eq_without_eq)]
             #[derive(Debug, PartialEq)]
             pub enum #match_error_name {
                 #(#error_variants),*
